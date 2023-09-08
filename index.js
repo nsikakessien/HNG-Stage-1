31757f03f1819ea50e0a0e880fb6af5ee821d50d
nsikak essien <nsikakessien66@gmail.com>
2023-09-08 17:16:37 +0100

    frontend page creation

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log(new Date().getUTCMilliseconds());
+
+const day = new Date().getDay();
+const utcTime = new Date().getUTCMilliseconds();
+const dayElement = document.querySelector(
+  "[data-testid='currentDayOfTheWeek']"
+);
+const timeElement = document.querySelector("[data-testid='currentUTCTime']");
+
+const returnedDay = (key) => {
+  switch (key) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+
+    default:
+      break;
+  }
+};
+
+dayElement.textContent = returnedDay(day);
+timeElement.textContent = utcTime;
